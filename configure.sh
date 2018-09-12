34ec566d2ec9f5c64d6dfdb2bd12a19da9afa80f
Zach <turtles.be.the.best@gmail.com>
2018-09-12 08:25:27 -0400

    set working selinux context for /etc/liboath

--- configure.sh
+++ configure.sh
@@ -5,10 +5,12 @@ yum -y update
 yum -y install epel-release
 yum -y install pam_oath
 
+chcon -R -u system_u -t ssh_home_t /etc/liboath
+
 cp files/users.oath /etc/liboath/users.oath
 
 patch /etc/pam.d/password-auth patches/00010-password-auth.patch
 patch /etc/pam.d/system-auth patches/00010-system-auth.patch
 
-semodule -i lib/pam_oath_context.pp
+#semodule -i lib/pam_oath_context.pp
 
