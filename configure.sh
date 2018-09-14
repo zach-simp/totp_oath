197143771eb291d3b5e03f5c614fbf9ead449486
Zach <turtles.be.the.best@gmail.com>
2018-09-14 12:56:16 -0400

    Adds user and group exceptions to pam stack for TOTP

--- configure.sh
+++ configure.sh
@@ -34,24 +34,39 @@ done
 # Changes selinux context for liboath dir and contained users.oath config to avoid selinux AVC denials 
 chcon -R -u system_u -t var_auth_t /etc/liboath
 
-cp files/users.oath /etc/liboath/users.oath
+cp files/users.oath /etc/liboath/
+cp files/exclude_users.oath /etc/liboath/
+cp files/exclude_groups.oath /etc/liboath/
 
-if grep -xq "auth     \[default=die\] pam_faillock.so authfail deny=5 even_deny_root audit unlock_time=900 root_unlock_time=60" "/etc/pam.d/password-auth"
+if grep -xq "auth	 \[success=2 default=ignore\] pam_listfile.so item=user sense=allow file=/etc/liboath/exclude_users.oath" "/etc/pam.d/password-auth"
 then
-	# patch with faillock in orig
-	patch -N -u /etc/pam.d/password-auth patches/00010-password-auth-nofail.patch
+	echo "Skipping..."
 else
-	# patch without failock in orig
-	patch -N -u /etc/pam.d/password-auth patches/00010-password-auth.patch
-	echo "You seem to have an out of date password-auth file."
-	echo "Patched for TOTP but does not have faillock enabled"
+	if grep -xq "auth     \[default=die\] pam_faillock.so authfail deny=5 even_deny_root audit unlock_time=900 root_unlock_time=60" "/etc/pam.d/password-auth"
+	then
+		# patch  with faillock in orig
+		patch -r - -N -u /etc/pam.d/password-auth patches/00010-password-auth-nofail.patch
+	else
+		# patch  without failock in orig
+		patch -r - -N -u /etc/pam.d/password-auth patches/00010-password-auth.patch
+		echo "You seem to have an out of date password-auth file."
+		echo "Patched for TOTP but does not have faillock enabled"
+	fi
 fi
 
-if grep -xq "auth     \[default=die\] pam_faillock.so authfail deny=5 even_deny_root audit unlock_time=900 root_unlock_time=60" "/etc/pam.d/system-auth"
+if grep -xq "auth	 \[success=2 default=ignore\] pam_listfile.so item=user sense=allow file=/etc/liboath/exclude_users.oath" "/etc/pam.d/system-auth"
 then
-	# patch with faillock in orig
-	patch -N -u /etc/pam.d/system-auth patches/00010-system-auth-nofail.patch
+	echo "Skipping..."
 else
-	# patch without failock in orig
-	patch -N -u /etc/pam.d/system-auth patches/00010-system-auth.patch
+	if grep -xq "auth     \[default=die\] pam_faillock.so authfail deny=5 even_deny_root audit unlock_time=900 root_unlock_time=60" "/etc/pam.d/system-auth"
+	then
+		# patch  with faillock in orig
+		patch -r - -N -u /etc/pam.d/system-auth patches/00010-system-auth-nofail.patch
+	else
+		# patch  without failock in orig
+		patch -r - -N -u /etc/pam.d/system-auth patches/00010-system-auth.patch
+	fi
 fi
+
+patch -r - -N -u /etc/pam.d/password-auth patches/00020-password-auth.patch
+patch -r - -N -u /etc/pam.d/system-auth patches/00020-system-auth.patch
